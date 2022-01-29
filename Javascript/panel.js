b124fca268c7327bb6de92b3b4bbf726cfb670c3
Juan Andrés Morena <64050263+JuaniiAndresM@users.noreply.github.com>
2022-01-28 23:19:18 -0300

    Panel

---
+++ Javascript/panel.js
@@ -0,0 +1,60 @@
+$(document).ready(function () {
+    let list = document.querySelectorAll('#panel-navbar li');
+
+    function activateLink(){
+        list.forEach((item) => 
+        item.classList.remove('hovered'));
+        this.classList.add('hovered');
+    }
+
+    list.forEach((item) => 
+    item.addEventListener('mouseover', activateLink));
+
+    document.getElementById('pre-loader').classList.toggle('load');
+    $('#panel-navbar').load('/SalioViaje/web/panel-navbar.html');
+});
+
+function navbar(){
+    let navbar = document.getElementById('panel-navbar');
+    let header = document.getElementById('header');
+    let panel = document.getElementById('panel');
+    
+    navbar.classList.toggle('active');
+    header.classList.toggle('active');
+    panel.classList.toggle('active');
+}
+
+function buscarPropiedades() {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("searchbar");
+    filter = input.value.toUpperCase();
+
+    table = document.getElementById("search-table");
+    tr = table.getElementsByTagName("tr");
+
+    
+
+    for (i = 0; i < tr.length; i++) {
+
+        a = 0;
+        encontrado = false;
+
+        do{
+            td = tr[i].getElementsByTagName("td")[a];
+            
+            if (td) {
+                txtValue = td.textContent || td.innerText;
+
+                if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                    tr[i].style.display = "";
+                    encontrado = true;
+                } else {
+                    tr[i].style.display = "none";
+                }
+            }
+
+            a++;
+        }while(a != 4 && encontrado == false);            
+            
+    }
+}
---
+++ Javascript/settings.js
@@ -0,0 +1,100 @@
+function settings(setting_name){
+    document.getElementById('settings-menu').classList.toggle('active');
+
+    $('#editar-info').hide();
+    $('#editar-contra').hide();
+    $('#administrar-usuarios').hide();
+    $('#editar-idioma').hide();
+    $('#configuracion-panel').hide();
+
+    switch(setting_name){
+        case 1:
+            $('#editar-info').show();
+            break;
+
+        case 2:
+            $('#editar-contra').show();
+
+            $('#passwd1').val("");
+            $('#passwd2').val("");
+            $('#passwd3').val("");
+
+            //Reset Inputs and Password Eye.
+
+            $('#passwd1').attr('type', 'password');
+            $('#passeye1').html('<i class="fas fa-eye-slash"></i>');
+            $('#passeye1').attr('class','hidden');
+
+            $('#passwd2').attr('type', 'password');
+            $('#passeye2').html('<i class="fas fa-eye-slash"></i>');
+            $('#passeye2').attr('class','hidden');
+
+            $('#passwd3').attr('type', 'password');
+            $('#passeye3').html('<i class="fas fa-eye-slash"></i>');
+            $('#passeye3').attr('class','hidden');
+            break;
+
+        case 3:
+            $('#administrar-usuarios').show();
+            break;
+
+        case 4:;
+            $('#editar-idioma').show();
+            break;
+
+        case 5:
+            $('#configuracion-panel').show();
+            break;
+        
+        default:
+            $('#editar-info').hide();
+            $('#editar-contra').hide();
+            $('#administrar-usuarios').hide();
+            $('#editar-idioma').hide();
+            $('#configuracion-panel').hide();
+            break;
+
+    }
+}
+
+function close_settings(){
+    document.getElementById('settings-menu').classList.toggle('active');
+}
+
+function passwd(num_input){
+    switch(num_input){
+        case 1:
+            if($('#passeye1').hasClass('show')){
+                $('#passwd1').attr('type', 'password');
+                $('#passeye1').html('<i class="fas fa-eye-slash"></i>');
+                $('#passeye1').attr('class','hidden');
+            }else{
+                $('#passwd1').attr('type', 'text');
+                $('#passeye1').html('<i class="fas fa-eye"></i>');
+                $('#passeye1').attr('class','show');
+            }
+            break;
+        case 2:
+            if($('#passeye2').hasClass('show')){
+                $('#passwd2').attr('type', 'password');
+                $('#passeye2').html('<i class="fas fa-eye-slash"></i>');
+                $('#passeye2').attr('class','hidden');
+            }else{
+                $('#passwd2').attr('type', 'text');
+                $('#passeye2').html('<i class="fas fa-eye"></i>');
+                $('#passeye2').attr('class','show');
+            }
+            break;
+        case 3:
+            if($('#passeye3').hasClass('show')){
+                $('#passwd3').attr('type', 'password');
+                $('#passeye3').html('<i class="fas fa-eye-slash"></i>');
+                $('#passeye3').attr('class','hidden');
+            }else{
+                $('#passwd3').attr('type', 'text');
+                $('#passeye3').html('<i class="fas fa-eye"></i>');
+                $('#passeye3').attr('class','show');
+            }
+            break;
+    }
+}
