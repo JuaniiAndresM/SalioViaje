92eb7b69c0f2cf6bb7aa333606e1fa742289c967
Firpito <medicenfirpito@gmail.com>
2022-01-28 23:18:30 -0300

    BD

--- Database/salioviaje.sql
+++ Database/salioviaje.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.26, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.24, for Win64 (x86_64)
 --
--- Host: 127.0.0.1    Database: salioviaje
+-- Host: localhost    Database: salioviaje
 -- ------------------------------------------------------
--- Server version	8.0.26
+-- Server version	8.0.24
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -24,13 +24,13 @@ DROP TABLE IF EXISTS `empresas`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `empresas` (
   `ID` int NOT NULL AUTO_INCREMENT,
-  `RUT` varchar(12) COLLATE utf8_spanish_ci NOT NULL,
-  `Nombre_C` varchar(200) COLLATE utf8_spanish_ci NOT NULL,
-  `Razon_S` varchar(200) COLLATE utf8_spanish_ci NOT NULL,
-  `Nro_MTOP` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
-  `Pass_MTOP` varchar(300) COLLATE utf8_spanish_ci NOT NULL,
+  `RUT` varchar(12) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+  `Nombre_C` varchar(200) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+  `Razon_S` varchar(200) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+  `Nro_MTOP` varchar(100) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+  `Pass_MTOP` varchar(300) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
   `Usuario_ID` int NOT NULL,
-  `Tipo_Usuario` varchar(3) COLLATE utf8_spanish_ci NOT NULL,
+  `Tipo_Usuario` varchar(3) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
   PRIMARY KEY (`ID`),
   KEY `IDUsuario_idx` (`Usuario_ID`),
   CONSTRAINT `IDUsu` FOREIGN KEY (`Usuario_ID`) REFERENCES `usuarios` (`ID`)
@@ -86,18 +86,18 @@ DROP TABLE IF EXISTS `usuarios`;
 CREATE TABLE `usuarios` (
   `ID` int NOT NULL AUTO_INCREMENT,
   `Tipo_Usuario` varchar(3) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
-  `CI` varchar(8) COLLATE utf8_spanish_ci NOT NULL,
-  `Email` varchar(300) COLLATE utf8_spanish_ci NOT NULL,
-  `Nombre` varchar(45) COLLATE utf8_spanish_ci NOT NULL,
-  `Apellido` varchar(45) COLLATE utf8_spanish_ci NOT NULL,
-  `Direccion` varchar(45) COLLATE utf8_spanish_ci NOT NULL,
-  `Barrio` varchar(45) COLLATE utf8_spanish_ci NOT NULL,
-  `Departamento` varchar(45) COLLATE utf8_spanish_ci NOT NULL,
-  `Telefono` varchar(9) COLLATE utf8_spanish_ci NOT NULL,
-  `Agencia_C` varchar(200) COLLATE utf8_spanish_ci DEFAULT NULL,
-  `PIN` varchar(4) COLLATE utf8_spanish_ci NOT NULL,
+  `CI` varchar(8) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+  `Email` varchar(300) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+  `Nombre` varchar(45) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+  `Apellido` varchar(45) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+  `Direccion` varchar(45) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+  `Barrio` varchar(45) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+  `Departamento` varchar(45) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+  `Telefono` varchar(9) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+  `Agencia_C` varchar(200) CHARACTER SET utf8 COLLATE utf8_spanish_ci DEFAULT NULL,
+  `PIN` varchar(4) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3 COLLATE=utf8_spanish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb3 COLLATE=utf8_spanish_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -119,15 +119,15 @@ DROP TABLE IF EXISTS `vehiculos`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `vehiculos` (
   `ID` int NOT NULL AUTO_INCREMENT,
-  `Matricula` varchar(7) COLLATE utf8_spanish_ci NOT NULL,
-  `Marca` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
-  `Modelo` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
-  `Combustible` varchar(45) COLLATE utf8_spanish_ci NOT NULL,
+  `Matricula` varchar(7) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+  `Marca` varchar(100) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+  `Modelo` varchar(100) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+  `Combustible` varchar(45) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
   `Capacidad` int NOT NULL,
   `Equipaje` int NOT NULL,
   `PetFriendly` int NOT NULL,
-  `RUT_EM` varchar(12) COLLATE utf8_spanish_ci NOT NULL,
-  `RUT_EC` varchar(12) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `RUT_EM` varchar(12) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+  `RUT_EC` varchar(12) CHARACTER SET utf8 COLLATE utf8_spanish_ci DEFAULT NULL,
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3 COLLATE=utf8_spanish_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -145,6 +145,30 @@ UNLOCK TABLES;
 --
 -- Dumping routines for database 'salioviaje'
 --
+/*!50003 DROP PROCEDURE IF EXISTS `register` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_AUTO_VALUE_ON_ZERO' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `register`(tipoUsuario varchar(3), ci int, email varchar(45), nombre varchar(45), apellido varchar(45),direccion varchar(45),barrio varchar(45), departamento varchar(45),telefono int, pin int)
+BEGIN
+
+IF tipoUsuario = "PAX" THEN
+	INSERT INTO `salioviaje`.`usuarios` (`Tipo_Usuario`, `CI`, `Email`, `Nombre`, `Apellido`, `Direccion`, `Barrio`, `Departamento`, `Telefono`, `PIN`) VALUES (tipoUsuario, ci, email, nombre, apellido, direccion, barrio, departamento, telefono, pin);
+ELSE 
+	SELECT 1;
+END IF;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -155,4 +179,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-01-28 12:15:30
+-- Dump completed on 2022-01-28 23:17:06
