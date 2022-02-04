cdaa4444e17eae6fb19ff619f36629f2b9905030
Juan Andrés Morena <64050263+JuaniiAndresM@users.noreply.github.com>
2022-02-03 23:05:59 -0300

    Validaciones

--- PHP/validaciones.php
+++ PHP/validaciones.php
@@ -57,7 +57,7 @@ class validaciones
 		}
 	}
 
-	private function validar_formulario_usuario_TTA_PAX_ASE($datos){
+	private function validar_formulario_usuario($datos){
 
 		$VALIDACION = array();
 		$DATOS_VACIOS = null;
@@ -127,231 +127,63 @@ class validaciones
 		if($DATOS_VACIOS == null && $errores == 0) { return true; } elseif ($DATOS_VACIOS != null) { return $DATOS_VACIOS; } else { return json_encode($VALIDACION);}
 
 	}
+
+	private function validar_formulario_empresa($datos){
+
+
+		$VALIDACION = array();
+		$DATOS_VACIOS = null;
+		$TIENE_MTOP = null;
+		$errores = 0;
+
+		foreach (json_decode($datos) as $clave => $valor){
+     		if ($valor != null || $valor != '' && $clave != 'VEHICULOS') {
+     			switch ($clave) {
+     				case 'RUT':
+     						$RUT = preg_match($this->PATTERN_RUT, $valor);
+     						$VALIDACION['RUT'] = $RUT;
+     					break;
+     				case 'NOMBRE_COMERCIAL':
+     						$NOMBRE_COMERCIAL = preg_match($this->PATTERN_NOMBRES, $valor);
+     						$VALIDACION['NOMBRE_COMERCIAL'] = $NOMBRE_COMERCIAL;
+     					break;
+     				case 'RAZON_SOCIAL':
+     						$RAZON_SOCIAL = preg_match($this->PATTERN_RAZON_SOCIAL, $valor);
+     						$VALIDACION['RAZON_SOCIAL'] = $RAZON_SOCIAL;
+     					break;
+     				case 'NUMERO_MTOP':
+     						if ($valor != null) {
+     							$TIENE_MTOP = 1;
+     							$MTOP = preg_match($this->PATTERN_NUMERO_MTOP, $valor);
+     							$VALIDACION['MTOP'] = $MTOP;
+     						}else{$TIENE_MTOP = 0;}
+     					break;
+     				case 'PASSWORD_MTOP':
+     						if ($TIENE_MTOP == 1) {
+     					    	$PASSWORD_MTOP = preg_match($this->PATTERN_PASSWORD_MTOP, $valor);
+     							$VALIDACION['PASSWORD_MTOP'] = $PASSWORD_MTOP;
+     						}
+     					break;
+     			}
+     		}
+		}
+
+		if (count($VALIDACION) == 3 && $TIENE_MTOP == 0) {
+			$DATOS_VACIOS = null;
+		}else if(count($VALIDACION) != 5){
+			$DATOS_VACIOS = "Err-1";
+		}
+
+		foreach ($VALIDACION as $clave => $valor){
+     		if ($valor  == 0) {
+     			$errores++;
+     		}
+		}
+
+		if($DATOS_VACIOS == null && $errores == 0) { return true; } elseif ($DATOS_VACIOS != null) { return $DATOS_VACIOS; } else { return json_encode($VALIDACION);}
+
+	}
 	
-	private function validar_formulario_usuario_CHO($datos){
-
-
-		$VALIDACION = array();
-		$DATOS_VACIOS = null;
-		$errores = 0;
-
-		foreach (json_decode($datos) as $clave => $valor){
-     		if ($valor != null || $valor != '') {
-     			switch ($clave) {
-     				case 'RUT':
-     						$RUT = preg_match($this->PATTERN_RUT, $valor);
-     						$VALIDACION['RUT'] = $RUT;
-     					break;
-     				case 'NOMBRE':
-     						$NOMBRE = preg_match($this->PATTERN_NOMBRES, $valor);
-     						$VALIDACION['NOMBRE'] = $NOMBRE;
-     					break;
-     				case 'APELLIDO':
-     						$APELLIDO = preg_match($this->PATTERN_NOMBRES, $valor);
-     						$VALIDACION['APELLIDO'] = $APELLIDO;
-     					break;
-     				case 'CORREO':
-     						$MAIL = preg_match($this->PATTERN_MAIL, $valor);
-     						$VALIDACION['MAIL'] = $MAIL;
-     					break;
-     				case 'DIRECCION':
-     						$DIRECCION = preg_match($this->PATTERN_DIRECCION, $valor);
-     						$VALIDACION['DIRECCION'] = $DIRECCION;
-     					break;
-     				case 'BARRIO':
-     						$BARRIO = preg_match($this->PATTERN_NOMBRES, $valor);
-     						$VALIDACION['BARRIO'] = $BARRIO;
-     					break;
-     				case 'DEPARTAMENTO':
-     						$DEPARTAMENTO = preg_match($this->PATTERN_NOMBRES, $valor);
-     						$VALIDACION['DEPARTAMENTO'] = $DEPARTAMENTO;
-     					break;
-     				case 'TELEFONO':
-     						$TELEFONO = preg_match($this->PATTERN_TELEFONO, $valor);
-     						$VALIDACION['TELEFONO'] = $TELEFONO;
-     					break;
-     				case 'PIN':
-     						$VALOR_PIN = $valor;
-     						$PIN = preg_match($this->PATTERN_PIN, $valor);
-     						$VALIDACION['PIN'] = $PIN;
-     					break;
-     				case 'RE-PIN':
-     						$RE_PIN = preg_match($this->PATTERN_PIN, $valor);
-     						if($VALOR_PIN == $valor){
-     							$PIN_MATCH = 1;
-     						}else{ $PIN_MATCH = 0; }
-     						$VALIDACION['PIN-MATCH'] = $PIN_MATCH;
-     					break;
-     				case 'AGENCIA_CONTRATISTA':
-     						if($valor == 0){
-     							$AGENCIA_CONTRATISTA = 0;
-     						}else{ $AGENCIA_CONTRATISTA = 1; }
-     						$VALIDACION['AGENCIA_CONTRATISTA'] = $AGENCIA_CONTRATISTA;
-     					break;
-     			}
-     		}
-		}
-
-		if (count($VALIDACION) != 11) {
-			$DATOS_VACIOS = "Hay datos vacios...";
-		}
-
-		foreach ($VALIDACION as $clave => $valor){
-     		if ($valor  == 0) {
-     			$errores++;
-     		}
-		}
-
-		if($DATOS_VACIOS == null && $errores == 0) { return true; } elseif ($DATOS_VACIOS != null) { return $DATOS_VACIOS; } else { return json_encode($VALIDACION);}
-
-	}
-
-	private function validar_formulario_usuario_ANF_AGT($datos){
-
-
-		$VALIDACION = array();
-		$DATOS_VACIOS = null;
-		$errores = 0;
-
-		foreach (json_decode($datos) as $clave => $valor){
-     		if ($valor != null || $valor != '') {
-     			switch ($clave) {
-     				case 'RUT':
-     						$RUT = preg_match($this->PATTERN_RUT, $valor);
-     						$VALIDACION['RUT'] = $RUT;
-     					break;
-     				case 'NOMBRE':
-     						$NOMBRE = preg_match($this->PATTERN_NOMBRES, $valor);
-     						$VALIDACION['NOMBRE'] = $NOMBRE;
-     					break;
-     				case 'APELLIDO':
-     						$APELLIDO = preg_match($this->PATTERN_NOMBRES, $valor);
-     						$VALIDACION['APELLIDO'] = $APELLIDO;
-     					break;
-     				case 'CORREO':
-     						$MAIL = preg_match($this->PATTERN_MAIL, $valor);
-     						$VALIDACION['MAIL'] = $MAIL;
-     					break;
-     				case 'DIRECCION':
-     						$DIRECCION = preg_match($this->PATTERN_DIRECCION, $valor);
-     						$VALIDACION['DIRECCION'] = $DIRECCION;
-     					break;
-     				case 'BARRIO':
-     						$BARRIO = preg_match($this->PATTERN_NOMBRES, $valor);
-     						$VALIDACION['BARRIO'] = $BARRIO;
-     					break;
-     				case 'DEPARTAMENTO':
-     						$DEPARTAMENTO = preg_match($this->PATTERN_NOMBRES, $valor);
-     						$VALIDACION['DEPARTAMENTO'] = $DEPARTAMENTO;
-     					break;
-     				case 'TELEFONO':
-     						$TELEFONO = preg_match($this->PATTERN_TELEFONO, $valor);
-     						$VALIDACION['TELEFONO'] = $TELEFONO;
-     					break;
-     				case 'PIN':
-     						$VALOR_PIN = $valor;
-     						$PIN = preg_match($this->PATTERN_PIN, $valor);
-     						$VALIDACION['PIN'] = $PIN;
-     					break;
-     				case 'RE-PIN':
-     						$RE_PIN = preg_match($this->PATTERN_PIN, $valor);
-     						if($VALOR_PIN == $valor){
-     							$PIN_MATCH = 1;
-     						}else{ $PIN_MATCH = 0; }
-     						$VALIDACION['PIN-MATCH'] = $PIN_MATCH;
-     					break;
-     			}
-     		}
-		}
-
-		if (count($VALIDACION) != 10) {
-			$DATOS_VACIOS = "Hay datos vacios...";
-		}
-
-
-		foreach ($VALIDACION as $clave => $valor){
-     		if ($valor  == 0) {
-     			$errores++;
-     		}
-		}
-
-		if($DATOS_VACIOS == null && $errores == 0) { return true; } elseif ($DATOS_VACIOS != null) { return $DATOS_VACIOS; } else { return json_encode($VALIDACION);}
-
-	}
-
-	private function validar_formulario_usuario_HTL($datos){
-
-
-		$VALIDACION = array();
-		$DATOS_VACIOS = null;
-		$errores = 0;
-
-		foreach (json_decode($datos) as $clave => $valor){
-			if ($valor != null || $valor != '') {
-				switch ($clave) {
-					case 'CI':
-							$CI = preg_match($this->PATTERN_CI, $valor);
-							$VALIDACION['CI'] = $CI;
-						break;
-					case 'NOMBRE':
-							$NOMBRE = preg_match($this->PATTERN_NOMBRES, $valor);
-							$VALIDACION['NOMBRE'] = $NOMBRE;
-						break;
-					case 'APELLIDO':
-							$APELLIDO = preg_match($this->PATTERN_NOMBRES, $valor);
-							$VALIDACION['APELLIDO'] = $APELLIDO;
-						break;
-					case 'CORREO':
-							$MAIL = preg_match($this->PATTERN_MAIL, $valor);
-							$VALIDACION['MAIL'] = $MAIL;
-						break;
-					case 'DIRECCION':
-							$DIRECCION = preg_match($this->PATTERN_DIRECCION, $valor);
-							$VALIDACION['DIRECCION'] = $DIRECCION;
-						break;
-					case 'BARRIO':
-							$BARRIO = preg_match($this->PATTERN_NOMBRES, $valor);
-							$VALIDACION['BARRIO'] = $BARRIO;
-						break;
-					case 'DEPARTAMENTO':
-							$DEPARTAMENTO = preg_match($this->PATTERN_NOMBRES, $valor);
-							$VALIDACION['DEPARTAMENTO'] = $DEPARTAMENTO;
-						break;
-					case 'TELEFONO':
-							$TELEFONO = preg_match($this->PATTERN_TELEFONO, $valor);
-							$VALIDACION['TELEFONO'] = $TELEFONO;
-						break;
-					case 'PIN':
-							$VALOR_PIN = $valor;
-							$PIN = preg_match($this->PATTERN_PIN, $valor);
-							$VALIDACION['PIN'] = $PIN;
-						break;
-					case 'RE-PIN':
-							$RE_PIN = preg_match($this->PATTERN_PIN, $valor);
-							if($VALOR_PIN == $valor){
-								$PIN_MATCH = 1;
-							}else{ $PIN_MATCH = 0; }
-							$VALIDACION['PIN-MATCH'] = $PIN_MATCH;
-						break;
-				}
-			}
-	   }
-
-		if (count($VALIDACION) != 10) {
-			$DATOS_VACIOS = "Hay datos vacios...";
-		}
-
-
-		foreach ($VALIDACION as $clave => $valor){
-     		if ($valor  == 0) {
-     			$errores++;
-     		}
-		}
-
-		if($DATOS_VACIOS == null && $errores == 0) { return true; } elseif ($DATOS_VACIOS != null) { return $DATOS_VACIOS; } else { return json_encode($VALIDACION);}
-
-	}
-
 	private function validar_formulario_empresa($datos){
 
 
