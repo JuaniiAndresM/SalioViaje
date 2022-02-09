f11631f124ae210464b067a38b8811cb9e67d591
Juan Andrés Morena <64050263+JuaniiAndresM@users.noreply.github.com>
2022-02-09 14:12:15 -0300

    Espera

--- Javascript/espera.js
+++ Javascript/espera.js
@@ -3,7 +3,11 @@ $(document).ready(function () {
 
     setTimeout(() => {
         steps(2);
-    }, 1000);
+    }, 2000);
+
+    setTimeout(() => {
+        steps(3);
+    }, 5000);
 });
 
 
@@ -11,12 +15,17 @@ $(document).ready(function () {
 
 
 function steps(step){
+    $("#step_1").hide();
+    $("#step_2").hide();
+    $("#step_3").hide();
+
     switch(step){
         case 1:
             $('.progress-line').css('width', '0%');
             $('.circle1').css('background-color', '#2b3179');
             $('.circle2').css('background-color', '#aaa');
             $('.circle3').css('background-color', '#aaa');
+            $("#step_1").show();
             break;
 
         case 2:
@@ -24,6 +33,7 @@ function steps(step){
             $('.circle1').css('background-color', '#2b3179');
             $('.circle2').css('background-color', '#2b3179');
             $('.circle3').css('background-color', '#aaa');
+            $("#step_2").show();
             break;
 
         case 3:
@@ -31,6 +41,7 @@ function steps(step){
             $('.circle1').css('background-color', '#2b3179');
             $('.circle2').css('background-color', '#2b3179');
             $('.circle3').css('background-color', '#2b3179');
+            $("#step_3").show();
             break;
 
     }
