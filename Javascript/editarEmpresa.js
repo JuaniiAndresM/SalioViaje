d98242fc240b7d14b12a5ace69074e5889a2ad0c
Firpito <medicenfirpito@gmail.com>
2022-02-26 21:29:36 -0300

    ...

---
+++ Javascript/editarEmpresa.js
@@ -0,0 +1,26 @@
+function guardar_cambios_vehiculos_panel(rut){
+	console.log(vehiculos)
+	console.log(" rut:"+rut)
+	$.ajax({
+        type: "POST",
+        url: "/SalioViaje/PHP/procedimientosForm.php",
+        data: {tipo:'guardar-vehiculos' ,vehiculos:JSON.stringify(vehiculos),rut:rut },
+          success: function (response) {
+  			console.log(response)
+          },
+    });
+}
+
+function vehiculos_vista_previa(vehiculo){
+	vehiculos.push(vehiculo)
+    $.ajax({
+      type: "POST",
+      url: "/SalioViaje/PHP/Tablas/agregarVehiculo.php",
+      data: {datos:JSON.stringify(vehiculo) },
+      success: function (response) {
+        $('.vehiculos').append(response);
+        $('#no-vehicle').hide();
+        reset_vehicle_inputs();
+     },
+  });
+}
