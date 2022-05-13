cff20efeb1777d90a149e0f45d8a1bce152a5259
Firpito <medicenfirpito@gmail.com>
2022-05-13 02:12:02 -0300

    ....

--- PHP/procedimientosBD.php
+++ PHP/procedimientosBD.php
@@ -1159,7 +1159,7 @@ class procedimientosBD
             $stmt->store_result();
             $stmt->bind_result($barrios);
             while ($stmt->fetch()) {
-                $barrios[] = $barrios;
+                $barrios->array_push($barrios);
             }
         }
         $stmt->close();
