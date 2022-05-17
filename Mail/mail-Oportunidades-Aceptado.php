577037f7946620dc947ca2daed79b8ce125c7c10
Firpito <medicenfirpito@gmail.com>
2022-05-16 22:33:13 -0300

    ...

--- Mail/mail-Oportunidades-Aceptado.php
+++ Mail/mail-Oportunidades-Aceptado.php
@@ -21,7 +21,8 @@ $id = $_POST['id_viaje'];
 
 $datos_oportunidad = $bd->traer_oportunidades_por_id($id);
 
-echo json_encode($datos_oportunidad);
+$datos_comprador =  $bd->info_usuario_profile($id);
+
 //
 /*------------------------------------------------------------------------------------------*/
 
@@ -79,10 +80,10 @@ $mail->Body    = '    <div class="mail" style="max-width: 600px; background: whi
                 <td>
                     <div class="mail-content" style="width: 500px; margin: 20px auto; background: #fff; font-family: Montserrat; color: #3844bc;">
                         <h1 style="font-size: 20px;">Información del Transportista</h1>
-                        <p style="font-size: 14px;"><b style="color: #444; margin-right: 5px;">Nombre: </b>Juan Andrés</p>
-                        <p style="font-size: 14px;"><b style="color: #444; margin-right: 5px;">Apellido: </b>Morena</p>
-                        <p style="font-size: 14px;"><b style="color: #444; margin-right: 5px;">Teléfono: </b><a href="tel:098234717" style="text-decoration: none; color: #3844bc;">098234717</a></p>
-                        <p style="font-size: 14px;"><b style="color: #444; margin-right: 5px;">Correo Electrónico: </b>juandres2003@gmail.com</p>
+                        <p style="font-size: 14px;"><b style="color: #444; margin-right: 5px;">Nombre: </b>'.$datos_oportunidad['NOMBRE'].'</p>
+                        <p style="font-size: 14px;"><b style="color: #444; margin-right: 5px;">Apellido: </b>'.$datos_oportunidad['APELLIDO'].'</p>
+                        <p style="font-size: 14px;"><b style="color: #444; margin-right: 5px;">Teléfono: </b><a href="tel:'.$datos_oportunidad['TELEFONO'].'" style="text-decoration: none; color: #3844bc;">'.$datos_oportunidad['TELEFONO'].'</a></p>
+                        <p style="font-size: 14px;"><b style="color: #444; margin-right: 5px;">Correo Electrónico: </b>'.$mail_tta.'</p>
                     </div>
                 </td>
             </tr>
@@ -149,10 +150,10 @@ $mail->Body    = '    <div class="mail" style="max-width: 600px; background: whi
                                     <td>
                                         <div class="mail-content" style="width: 500px; margin: 20px auto; background: #fff; font-family: Montserrat; color: #3844bc;">
                                             <h1 style="font-size: 20px;">Información del Transportista</h1>
-                                            <p style="font-size: 14px;"><b style="color: #444; margin-right: 5px;">Nombre: </b>Juan Andrés</p>
-                                            <p style="font-size: 14px;"><b style="color: #444; margin-right: 5px;">Apellido: </b>Morena</p>
-                                            <p style="font-size: 14px;"><b style="color: #444; margin-right: 5px;">Teléfono: </b><a href="tel:098234717" style="text-decoration: none; color: #3844bc;">098234717</a></p>
-                                            <p style="font-size: 14px;"><b style="color: #444; margin-right: 5px;">Correo Electrónico: </b>juandres2003@gmail.com</p>
+                                            <p style="font-size: 14px;"><b style="color: #444; margin-right: 5px;">Nombre: </b>'.$datos_oportunidad['NOMBRE'].'</p>
+                                            <p style="font-size: 14px;"><b style="color: #444; margin-right: 5px;">Apellido: </b>'.$datos_oportunidad['APELLIDO'].'</p>
+                                            <p style="font-size: 14px;"><b style="color: #444; margin-right: 5px;">Teléfono: </b><a href="tel:'.$datos_oportunidad['TELEFONO'].'" style="text-decoration: none; color: #3844bc;">'.$datos_oportunidad['TELEFONO'].'</a></p>
+                                            <p style="font-size: 14px;"><b style="color: #444; margin-right: 5px;">Correo Electrónico: </b>'.$mail_tta.'</p>
                                         </div>
                                     </td>
                                 </tr>
@@ -160,10 +161,10 @@ $mail->Body    = '    <div class="mail" style="max-width: 600px; background: whi
                                     <td>
                                         <div class="mail-content" style="width: 500px; margin: 20px auto; background: #fff; font-family: Montserrat; color: #3844bc;">
                                             <h1 style="font-size: 20px;">Información del Pasajero</h1>
-                                            <p style="font-size: 14px;"><b style="color: #444; margin-right: 5px;">Nombre: </b>Juan Andrés</p>
-                                            <p style="font-size: 14px;"><b style="color: #444; margin-right: 5px;">Apellido: </b>Morena</p>
-                                            <p style="font-size: 14px;"><b style="color: #444; margin-right: 5px;">Teléfono: </b><a href="tel:098234717" style="text-decoration: none; color: #3844bc;">098234717</a></p>
-                                            <p style="font-size: 14px;"><b style="color: #444; margin-right: 5px;">Correo Electrónico: </b>juandres2003@gmail.com</p>
+                                            <p style="font-size: 14px;"><b style="color: #444; margin-right: 5px;">Nombre: </b>'.$datos_comprador['NOMBRE'].'</p>
+                                            <p style="font-size: 14px;"><b style="color: #444; margin-right: 5px;">Apellido: </b>'.$datos_comprador['APELLIDO'].'</p>
+                                            <p style="font-size: 14px;"><b style="color: #444; margin-right: 5px;">Teléfono: </b><a href="tel:'.$datos_comprador['TELEFONO'].'" style="text-decoration: none; color: #3844bc;">'.$datos_comprador['TELEFONO'].'</a></p>
+                                            <p style="font-size: 14px;"><b style="color: #444; margin-right: 5px;">Correo Electrónico: </b>'.$datos_comprador['EMAIL'].'</p>
                                         </div>
                                     </td>
                                 </tr>
@@ -231,10 +232,10 @@ $mail->Body    = '<div class="mail" style="max-width: 600px; background: white;"
                 <td>
                     <div class="mail-content" style="width: 500px; margin: 20px auto; background: #fff; font-family: Montserrat; color: #3844bc;">
                         <h1 style="font-size: 20px;">Información del Pasajero</h1>
-                        <p style="font-size: 14px;"><b style="color: #444; margin-right: 5px;">Nombre: </b>Juan Andrés</p>
-                        <p style="font-size: 14px;"><b style="color: #444; margin-right: 5px;">Apellido: </b>Morena</p>
-                        <p style="font-size: 14px;"><b style="color: #444; margin-right: 5px;">Teléfono: </b><a href="tel:098234717" style="text-decoration: none; color: #3844bc;">098234717</a></p>
-                        <p style="font-size: 14px;"><b style="color: #444; margin-right: 5px;">Correo Electrónico: </b>juandres2003@gmail.com</p>
+                        <p style="font-size: 14px;"><b style="color: #444; margin-right: 5px;">Nombre: </b>'.$datos_comprador['NOMBRE'].'</p>
+                        <p style="font-size: 14px;"><b style="color: #444; margin-right: 5px;">Apellido: </b>'.$datos_comprador['APELLIDO'].'</p>
+                        <p style="font-size: 14px;"><b style="color: #444; margin-right: 5px;">Teléfono: </b><a href="tel:'.$datos_comprador['TELEFONO'].'" style="text-decoration: none; color: #3844bc;">'.$datos_comprador['TELEFONO'].'</a></p>
+                        <p style="font-size: 14px;"><b style="color: #444; margin-right: 5px;">Correo Electrónico: </b>'.$datos_comprador['EMAIL'].'</p>
                     </div>
                 </td>
             </tr>
