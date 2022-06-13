4a96c5d6ee0bbaf4a56b5c8d069141fefc72c7fa
Juan Andrés Morena <64050263+JuaniiAndresM@users.noreply.github.com>
2022-06-13 16:27:07 -0300

    SOlicitudCotizacion

---
+++ Javascript/solicitudCotizacion.js
@@ -0,0 +1,70 @@
+$(document).ready(function () {
+    var ID = $("#id_get").val();
+    var ID_Slice = ID.slice(0,-1);
+    
+        // if($("#expired_get").val() == "1"){
+        //     solicitud_response = "E";
+        // }else{
+        //     solicitud_response = ID.charAt(ID.length-1);
+        // }
+
+        solicitud_response = ID.charAt(ID.length-1);
+
+        switch(solicitud_response){
+            case "A":
+                // cotizacion_aprobada(ID_Slice)
+                solicitud(1,ID);
+                break;
+            case "R":
+                // cotizacion_rechazada(ID_Slice)
+                solicitud(2,ID);
+                break;
+            case "E":
+                solicitud(3,ID);
+                break;
+        }
+    
+    
+});
+
+
+
+
+function solicitud(data, ID){
+    var ID_Slice = ID.slice(0,-1);
+
+    switch(data){
+        case 1:
+            $("#icon").attr('class', 'fas fa-check');
+            $("#icon").css('color', 'rgb(116, 212, 129)');
+
+            $("#info_1").text("Aceptada");
+            $("#info_1").css('color', 'rgb(116, 212, 129)');
+
+            $("#info_2").text("Cotización N° "+ ID_Slice +" Aceptada.");
+            $("#info_3").text("Pronto serás contactado por el pasajero para coordinar el pago del viaje.");
+            break;
+
+        case 2:
+            $("#icon").attr('class', 'fas fa-times');
+            $("#icon").css('color', 'rgb(255, 91, 91)');
+
+            $("#info_1").text("Rechazada");
+            $("#info_1").css('color', 'rgb(255, 91, 91)');
+
+            $("#info_2").text("Cotización N° "+ ID_Slice +" Rechazada.");
+            $("#info_3").text("Pronto nos pondremos en contacto para saber el motivo de tu rechazo.");
+            break;
+
+        case 3:
+            $("#icon").attr('class', 'far fa-clock');
+            $("#icon").css('color', 'rgb(255, 211, 91)');
+
+            $("#info_1").text("Expirado");
+            $("#info_1").css('color', 'rgb(255, 211, 91)');
+
+            $("#info_2").text("Este link ha caducado.");
+            $("#info_3").text("De ser un error ponte en contacto con los administradores.");
+            break;
+    }
+}
