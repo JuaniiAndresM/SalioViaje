c45c20e81d901964cd6dcb042d6a8dfa2473e96c
Juan Andrés Morena <64050263+JuaniiAndresM@users.noreply.github.com>
2022-07-09 14:10:32 -0300

    Ofertas y Promo Changes

--- Panel/Ofertas.php
+++ Panel/Ofertas.php
@@ -146,20 +146,42 @@ session_set_cookie_params($ttl);
                         </div>
                         <p>Cargar Oferta</p>
                     </a>
-                    <a class="card button" href="">
+                    <?php 
+                    if($_SESSION['datos_usuario']['TIPO_USUARIO'] == "TTA"){
+                      echo `
+                      <a class="card button" href="">
                         <div class="number">
                             <h2>+</h2>
                             <i class="fa-solid fa-handshake"></i>
                         </div>
                         <p>Convenios</p>
-                    </a>
-                    <a class="card button" href="">
+                      </a>
+                      <a class="card button" href="">
+                          <div class="number">
+                              <h2>-</h2>
+                              <i class="fa-solid fa-hotel"></i>
+                          </div>
+                          <p>Modulos Especiaes:<br>Fiestas y Hoteles</p>
+                      </a>`;
+                    }else if($_SESSION['datos_usuario']['TIPO_USUARIO'] == "AGT" || $_SESSION['datos_usuario']['TIPO_USUARIO'] == "HTL" || $_SESSION['datos_usuario']['TIPO_USUARIO'] == "ANF" || $_SESSION['datos_usuario']['TIPO_USUARIO'] == "ADM"){
+                      echo `
+                      <a class="card button" href="">
                         <div class="number">
-                            <h2>-</h2>
-                            <i class="fa-solid fa-hotel"></i>
+                            <h2>+</h2>
+                            <i class="fa-solid fa-bullhorn"></i>
                         </div>
-                        <p>Modulos Especiaes:<br>Fiestas y Hoteles.</p>
-                    </a>
+                        <p>Nueva Promo</p>
+                      </a>
+                      <a class="card button" href="">
+                          <div class="number">
+                              <h2>-</h2>
+                              <i class="fa-solid fa-sack-dollar"></i>
+                          </div>
+                          <p>Incentivos</p>
+                      </a>`;
+                    }
+                    ?>
+                    
                     <a class="card button" href="Vouchers">
                         <div class="number">
                             <h2>-</h2>
